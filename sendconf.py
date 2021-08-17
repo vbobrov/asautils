dc78aecd64fd5d2969c9a376c1c38adca8e59471
Viktor Bobrov <vbobrov@gmail.com>
2021-08-17 12:47:29 -0400

    added comments. used with for file ops

--- sendconf.py
+++ sendconf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import logging
 import sys
 import requests
@@ -16,6 +17,7 @@ from getpass import getpass
 def send_exec_command(command):
 	global asa_session,asa_base_url
 	logging.debug(f"Attempting to execute: {command}")
+	# Since the command is specified in the URL, we need to URL escape it
 	r=asa_session.get(f"{asa_base_url}/exec/{quote(command,safe='')}",allow_redirects=False)
 	r.raise_for_status()
 	logging.debug(f"Response received:\n{r.text}")
@@ -29,6 +31,7 @@ def post_data(url,data):
 	http_client.HTTPConnection.debuglevel = 0
 	r=asa_session.post(f"{asa_base_url}/{url}",data=data,allow_redirects=False)
 	r.raise_for_status()
+	# Restoring debug level
 	http_client.HTTPConnection.debuglevel = debug_level
 	logging.debug(f"Response received:\n{r.text}")
 	return(r.text)
@@ -36,6 +39,7 @@ def post_data(url,data):
 def get_file_md5(asa_file_name):
 	verify_output=send_exec_command(f"verify /md5 disk0:/{asa_file_name}")
 	try:
+		# Looking for a sequence of 32 hex digits, assuming it's the MD5 hash
 		return(re.match(r".*([0-9a-z]{32}).*",verify_output,re.S)[1])
 	except:
 		return("unknown")
@@ -58,11 +62,24 @@ debug_level=[logging.WARNING,logging.INFO,logging.DEBUG][args.d-1]
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',level=debug_level)
 
 asa_base_url=f"https://{args.a}/admin"
+
 logging.debug("Validating arguments")
+
 errors=""
+
+# This option tells the tool to output a Windows or Linux commands to execute action.
+# When specified the tool will not communicate the the ASA at all.
+# Additionally, this option will disable checking if any files such as uploads or certificates exist on disk.
+# The tool generates only Linux commands through out the code.
+# A simple find/replace is used at the end to translate Linux commands into their Windows alternatives
 manual=args.m
+
+# If -x option is specified all requests and generated manual commands use HTTP Basic authentication.
 asdm_auth=not args.x
 
+# Parsing identity certificate option -i
+# Parsed data is stored in a list. All file data is loaded into a dict to avoid re-reading the same file again.
+# When manual option is specified, only file names are stored.
 id_certs=[]
 if args.i:
 	for id_cert in args.i:
@@ -80,14 +97,16 @@ if args.i:
 			if manual:
 				id_certs.append({"tp_name":tp_name,"pkcs12_file":pkcs12_file,"pkcs12_password":pkcs12_password})
 			else:
-				f=open(pkcs12_file,"rb")
-				pkcs12_data=f.read()
-				f.close()
+				with open(pkcs12_file,"rb") as f:
+					pkcs12_data=f.read()
 				id_certs.append({"tp_name":tp_name,"pkcs12_data":pkcs12_data,"pkcs12_password":pkcs12_password})
 		except:
 			errors+=f"Unable to read {pkcs12_file}\n"
 			break
 
+# Parsing CA certificate option -t
+# Parsed data is stored in a list. All file data is loaded into a dict to avoid re-reading the same file again.
+# When manual option is specified, only file names are stored.
 ca_certs=[]
 if args.t:
 	for ca_cert in args.t:
@@ -104,14 +123,16 @@ if args.t:
 			if manual:
 				ca_certs.append({"tp_name":tp_name,"ca_file":ca_file})
 			else:
-				f=open(ca_file,"r")
-				ca_data=f.read()
-				f.close()
-				ca_certs.append({"tp_name":tp_name,"ca_data":ca_data})
+				with open(ca_file,"r") as f:
+					ca_data=f.read()
+					ca_certs.append({"tp_name":tp_name,"ca_data":ca_data})
 		except:
 			errors+=f"Unable to read {ca_file}\n"
 			break
 
+# Parsing upload option -f
+# Parsed data is stored in a list. All file data is loaded into a dict to avoid re-reading the same file again.
+# When manual option is specified, only file names are stored.
 upload_files=[]
 if args.f:
 	for file in args.f:
@@ -129,14 +150,16 @@ if args.f:
 			if manual:
 				upload_files.append({"asa_file_name":asa_file_name,"local_file_name":local_file_name})
 			else:
-				f=open(local_file_name,"rb")
-				file_data=f.read()
-				f.close()
-				upload_files.append({"asa_file_name":asa_file_name,"file_data":file_data})
+				with open(local_file_name,"rb") as f:
+					file_data=f.read()
+					upload_files.append({"asa_file_name":asa_file_name,"file_data":file_data})
 		except:
 			errors+=f"Unable to open {local_file_name}\n"
 			break
 
+# Parsing config option -c
+# Parsed data is stored in a list. All file data is loaded into a dict to avoid re-reading the same file again.
+# When manual option is specified, only file names are stored.
 config_files=[]
 if args.c:
 	for file in args.c:
@@ -144,19 +167,20 @@ if args.c:
 			if manual:
 				config_files.append({"config_file_name":file})
 			else:
-				f=open(file,"r")
-				config_data=f.read()
-				f.close
-				config_files.append({"config_file_name":file,"config_data":config_data})
+				with open(file,"r") as f:
+					config_data=f.read()
+					config_files.append({"config_file_name":file,"config_data":config_data})
 		except:
 			errors+=f"Unable to open {file}\n"
 
 if not id_certs and not ca_certs and not upload_files and not config_files:
 	errors+="No task was specified\n"
 
+# Throw an exception if errors are found
 if errors:
 	parser.error(f"\n{errors}")
 
+# Prompt for username and/or password interactively if not specified in arguments
 if args.u:
 	username=args.u
 else:
@@ -167,18 +191,25 @@ if args.p:
 else:
 	password=getpass("ASDM Password: ")
 
+
 if manual:
 	windows_cli=""
 	linux_cli=""
+	# When manual operation is specified, any temporary files needed for Windows or Linux commands are stored in system's temp directory using a random suffix.
+	# Files are deleted after each operation
 	tmp_suffix=random.randint(100000,999999)
 	
 	if asdm_auth:
+		# If ASDM authentication is specified, an initial login to the ASA is required to get an authentication cookie.
+		# That cookie is used in all subsequent requests
 		linux_cli+=f'''# Login to the ASA
 curl -k -X POST -d "username={username}&password={password}&tgroup=DefaultADMINGroup&Login=Login" "https://{args.a}/+webvpn+/index.html" -A "ASDM/"  -b "webvpnlogin=1; tg=0RGVmYXVsdEFETUlOR3JvdXA=" -c /tmp/cj_{tmp_suffix}.txt
 '''
 		curl_url=args.a
 		curl_opt='-A "ASDM/"  -b /tmp/cj_{tmp_suffix}.txt -c /tmp/cj_{tmp_suffix}.txt'
 	else:
+		# With Basic authentication, the username and password are sent in every request.
+		# Basic authentication requires http server basic-auth-client ASDM command on the ASA. This command is present by default.
 		curl_url=f"{username}:{password}@{args.a}"
 		curl_opt='-A "ASDM"'
 else:
@@ -187,12 +218,17 @@ else:
 	requests_log=logging.getLogger("requests.packages.urllib3")
 	requests_log.setLevel(debug_level)
 	requests_log.propagate=True
+
+	# All requests will use a session. This simplifies handling of headers and cookies.
 	asa_session=requests.Session()
 
+	# Disable any certificate verification or errors.
 	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 	asa_session.verify=False
 
 	if asdm_auth:
+		# ASDM sends a specific cookie ot the ASA to enable for form-based authentication.
+		# After a successful authentication, webvpn cookie is return and is then used for subsequent requests
 		asa_session.cookies.set("tg","0RGVmYXVsdEFETUlOR3JvdXA=")
 		asa_session.cookies.set("webvpnlogin","1")
 		asa_session.headers.update({"User-Agent":"ASDM/"})
@@ -203,10 +239,13 @@ else:
 			sys.exit(1)
 		
 	else:
+		# Username and password for Basic authentication
 		asa_session.auth=(username,password)
 		asa_session.headers.update({"User-Agent":"ASDM"})
 		logging.debug("Validating basic credentials")
 		r=asa_session.get(f"{asa_base_url}/exec/show+version",allow_redirects=False)
+		# ASA returns either 400 or 401 depending on failure.
+		# If another code is return, the tool will throw an exception and exit.
 		if r.status_code==401:
 			logging.error("Basic credentials invalid")
 			sys.exit(1)
@@ -215,11 +254,15 @@ else:
 			sys.exit(1)
 		r.raise_for_status
 
-
+# PKCS12 files are always stored as binary. ASA can read the data from these files unchanged. 
+# In order to send this data to ASA, it has to be converted to "pastable" base64 encoding.
 for ca_cert in ca_certs:
 	tp_name=ca_cert["tp_name"]
 	if manual:
 		ca_file=ca_cert["ca_file"]
+		# In manual mode, the commands build up a temporary text file with the commands required to import PKCS12 file.
+		# All commands are then sent using curl.
+		# The temporary file is delete at the end.
 		linux_cli+=f'''
 # Import CA Cert from {ca_file} into {tp_name}
 echo no crypto ca trustpoint {tp_name} noconfirm >/tmp/{tp_name}_{tmp_suffix}.txt
@@ -243,11 +286,17 @@ quit
 """)
 		logging.info(f"Received response:\n{config_response}")
 
+# CA certificates can be stored in base64-encoded PEM files or binary DER files.
+# To avoid dependency on modules and tool to convert certificate files, this tool will only work with PEM files.
+# DER files need to be converted to PEM. With openssl, it can be done as follows: openssl x509 -inform der -in cert.der -out cert.pem
 for id_cert in id_certs:
 	tp_name=id_cert["tp_name"]
 	pkcs12_password=id_cert["pkcs12_password"]
 	if manual:
 		pkcs12_file=id_cert["pkcs12_file"]
+		# In manual mode, the commands build up a temporary text file with the commands required to import CA file.
+		# All commands are then sent using curl.
+		# The temporary file is delete at the end.
 		linux_cli+=f'''
 # Import ID Cert from {pkcs12_file} into {tp_name}
 base64 -b 64 -i {pkcs12_file} -o /tmp/{tp_name}_{tmp_suffix}.b64
@@ -262,6 +311,7 @@ rm /tmp/{tp_name}_{tmp_suffix}.txt /tmp/{tp_name}_{tmp_suffix}.b64
 	else:
 		pkcs12_data=id_cert["pkcs12_data"]
 		pkcs12_b64=b64encode(pkcs12_data).decode()
+		# Break a long base64 string into multiple lines, 64-characters each.
 		pkcs12_asa=re.sub(r"(.{64})","\\1\n",pkcs12_b64,0,re.DOTALL)
 		logging.info(f"Attempting to import PKCS12 identity certificate {tp_name}")
 		config_response=post_data("config",f"""no crypto ca trustpoint {tp_name} noconfirm
@@ -272,6 +322,9 @@ rm /tmp/{tp_name}_{tmp_suffix}.txt /tmp/{tp_name}_{tmp_suffix}.b64
 	""")
 		logging.info(f"Received response:\n{config_response}")
 
+# To avoid reuploading the same file multiple times, verify command is run first to get the MD5 has of the file.
+# This hash is then compared to the local file. If they're the same, the upload is skipped. MD5 is used to speed up processing.
+# In manual mode, there's to command generated to do MD5 comparison.
 for upload_file in upload_files:
 	asa_file_name=upload_file["asa_file_name"]
 	if manual:
@@ -283,7 +336,9 @@ curl -k "https://{curl_url}/admin/disk0/{asa_file_name}" {curl_opt} --data-binar
 	else:
 		file_data=upload_file["file_data"]
 		logging.debug(f"Getting MD5 checksum for {asa_file_name} from ASA")
+		# Get MD5 hash of the file from the ASA.
 		asa_md5=get_file_md5(asa_file_name)
+		# Calculate local MD5 hash.
 		local_md5=hashlib.md5(file_data).hexdigest()
 		logging.debug(f"ASA MD5 is {asa_md5}. Local MD5 is {local_md5}")
 		if local_md5!=asa_md5:
@@ -293,6 +348,9 @@ curl -k "https://{curl_url}/admin/disk0/{asa_file_name}" {curl_opt} --data-binar
 		else:
 			logging.info(f"{asa_file_name} is already on the device. Upload skipped.")
 
+# The ASA supports both exec and config commands in these files.
+# It is not necessary to supply configure terminal and exit commands.
+# There are certain commands on the ASA that take a few seconds to complete. Write memory command may not work in these cases when supplied in the same file.
 for config_file in config_files:
 	config_file_name=config_file["config_file_name"]
 	if manual:
@@ -312,6 +370,7 @@ if manual:
 # Delete Cookie Jar file
 rm /tmp/cj_{tmp_suffix}.txt
 '''
+	# Simple find and replace to convert Linux OS commands to Windows.
 	windows_cli=linux_cli.replace("rm /","del /").replace("/tmp/","%TEMP%\\").replace("cat ","type ").\
 		replace("base64 -b 64 -i","certutil -encode").replace(" -o "," ").replace("# ","REM ")
 	print(f"----------------------------- *nix cli -----------------------------\n{linux_cli}")
